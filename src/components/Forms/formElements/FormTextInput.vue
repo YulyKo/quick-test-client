f1fe7d41b2bc0cd52a81bd135f1e03b976b2a445
YulyKo <yuliakotenko1.0@gmail.com>
2020-11-10 16:31:44 +0200

    refactored FormTextInput

--- src/components/Forms/formElements/FormTextInput.vue
+++ src/components/Forms/formElements/FormTextInput.vue
@@ -8,31 +8,14 @@
             :class="{'form__container_input--error': !this.valid}"
             required>
     </div>
-    <form-error-messages :errors="this.errors"></form-error-messages>
   </div>
 </template>
 
 <script>
-// import {
-//   validName,
-//   setMessage,
-//   ERROR_MESSAGE_FOR_SO_LONG_NAME,
-// } from '@/utils/index';
-import FormErrorMessagesVue from './FormErrorMessages.vue';
-
-// ERROR_MESSAGE_FOR_INVALID_NAME,
 
 export default {
   name: 'FormTextInput',
   props: ['value', 'inputFieldType', 'valid'],
-  components: {
-    formErrorMessages: FormErrorMessagesVue,
-  },
-  data() {
-    return {
-      errors: [],
-    };
-  },
   computed: {
     getLengthOfErrors() {
       return true;
@@ -43,13 +26,7 @@ export default {
       },
       set(val) {
         this.$emit('input', val);
-        this.$emit('focusout', val);
       },
-    },
-  },
-  methods: {
-    validate() {
-      return 0;
     },
   },
 };
