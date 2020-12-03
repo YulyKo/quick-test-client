15f1b8e4d42473882fbb4bbe88ca30b13cd5f575
YulyKo <yuliakotenko1.0@gmail.com>
2020-12-03 21:21:49 +0200

    deleted extra files

--- src/components/Lists/CoursesList.vue
+++ src/components/Lists/CoursesList.vue
@@ -3,14 +3,6 @@
     <div v-for="(course, id) in courses" :key="id"
         class="list__card">
       <div class="list__card_nav">
-        <modal-window title="Введіть нову назву курсу" buttonName="Редагувати">
-          <editing-form></editing-form>
-        </modal-window>
-        <!-- <edit-button
-        buttonClasses="button button-edit">
-          <template v-slot:buttonName>Редагувати</template>
-        </edit-button> -->
-        <!-- <form-button type="button" classes="button">Редагувати</form-button> -->
         <delete-button
           classes="button button-del"
           type="button"
@@ -27,14 +19,10 @@
 <script>
 import { mapGetters } from 'vuex';
 import DeleteButtonVue from '@/mixins/UI/formElements/DeleteButton.vue';
-import ModalWindowVue from '../../mixins/UI/ModalWindow.vue';
-import FormVue from '../Forms/Form.vue';
 
 export default {
   components: {
     deleteButton: DeleteButtonVue,
-    modalWindow: ModalWindowVue,
-    editingForm: FormVue,
   },
   computed: {
     ...mapGetters({
