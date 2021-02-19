657d8489a97703e8ccb810ebf1abf332f43ff0eb
YulyKo <yuliakotenko1.0@gmail.com>
2021-02-19 16:45:20 +0200

    auth/created componetns login & registration

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from 'src/app/components/auth/auth.component';
+import { LoginComponent } from 'src/app/components/auth/login/login.component';
+import { RegistrationComponent } from 'src/app/components/auth/registration/registration.component';
 
 @NgModule({
   declarations: [
     AuthComponent,
+    LoginComponent,
+    RegistrationComponent,
   ],
   imports: [
     CommonModule
