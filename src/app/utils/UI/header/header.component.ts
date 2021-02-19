abe05961b87aa53e5ef7e4ab5d6d3771a57d539e
YulyKo <yuliakotenko1.0@gmail.com>
2021-02-19 15:05:20 +0200

    website/added base header

--- src/app/utils/UI/header/header.component.ts
+++ src/app/utils/UI/header/header.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
   constructor() { }
+
+  ngOnInit(): void {}
+
+  isOpen: boolean = false;
+
+  changeMenuVisibility(): void {
+    if (this.isOpen) {
+      this.closeMenu();
+    } else {
+      this.openMenu();
+    }
+  }
+
+  openMenu(): void {
+    document.getElementById('page_header').style.backgroundColor = '#404040';
+    document.getElementById('header_menu').style.display = 'flex';
+    this.isOpen = true;
+  }
+
+  closeMenu(): void {
+    document.getElementById('page_header').style.backgroundColor = '#FFF';
+    document.getElementById('header_menu').style.display = 'none';
+    this.isOpen = false;
+  }
 
 }
