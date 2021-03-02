48736f74562f26bf45078bf6e994aeaa80cca860
YulyKo <yuliakotenko1.0@gmail.com>
2021-03-02 12:25:07 +0200

    auth/added new version of error messages

--- src/app/utils/ErrorsMessages.ts
+++ src/app/utils/ErrorsMessages.ts
@@ -2,6 +2,7 @@ export enum ErrorsMessages {
   ERROR_MESSAGE_FOR_REQUIRED_FIELD = 'Обов\'язкове поле',
   ERROR_MESSAGE_FOR_INVALID_MENTOR_NAME = 'І\'мя було веддено не коректно',
   ERROR_MESSAGE_FOR_SO_SHORT_MENTOR_NAME = 'Мінімальна довжина 3 символи',
+  ERROR_MESSAGE_FOR_SO_LONG_MENTOR_NAME = 'Максимальна довжина 20 символів',
   ERROR_MESSAGE_FOR_INVALID_EMAIL = 'Пошта введена не коректно',
   ERROR_MESSAGE_FOR_EXISTED_EMAIL = 'Аккаунт із такою поштою існує',
   ERROR_MASSAGE_FOR_INVALID_PASSWORD = 'Пароль має містити влику літеру, малу літеру та цифру',
