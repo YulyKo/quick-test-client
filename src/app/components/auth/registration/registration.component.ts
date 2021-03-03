2c5c426c71446d034179d4653a3025183a41eff0
YulyKo <yuliakotenko1.0@gmail.com>
2021-03-03 16:06:33 +0200

    auth/used new RegExp for password

--- src/app/components/auth/registration/registration.component.ts
+++ src/app/components/auth/registration/registration.component.ts
@@ -31,7 +31,7 @@ export class RegistrationComponent implements OnInit {
         password: ['', [ 
           Validators.required,
           Validators.minLength(8),
-          Validators.pattern(Patterns.PASSWORD_PATTERN) ]],
+          Validators.pattern(new RegExp(Patterns.PASSWORD_PATTERN)) ]],
         repeatPassword: ['', [
           Validators.required,
           Validators.minLength(8),
@@ -45,7 +45,7 @@ export class RegistrationComponent implements OnInit {
   onSubmit() {
     this.submited = true;
     console.log(this.form.value);
-    console.log(this.form.controls);
+    console.log(this.form.controls.password.errors.pattern);
   }
 
 }
