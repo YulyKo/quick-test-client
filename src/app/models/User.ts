ad5dfc8859d0877484ea7f3dced7927498e25b46
YulyKo <yuliakotenko1.0@gmail.com>
2021-03-05 13:48:11 +0200

    auth/added 'set' methods to class User

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,7 +1,19 @@
 export class User {
-  id: number;
   password: string;
   name: string;
   token?: string;
   email: string;
+
+  public setName(name : string) {
+    this.name = name;
+  }
+
+  public setPassword(password : string) {
+    this.password = password;
+  }
+
+  public setEmail(email : string) {
+    this.email = email;
+  }
+    
 }
