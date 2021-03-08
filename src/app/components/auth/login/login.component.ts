75902c4dcace9b800f002eb538ab6b3fc83c4dad
YulyKo <yuliakotenko1.0@gmail.com>
2021-03-08 16:33:13 +0200

    auth/added error messages by register in login component

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ErrorsMessages } from 'src/app/utils/ErrorsMessages';
+import { Patterns } from 'src/app/utils/Patterns';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +10,20 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   form: FormGroup;
-
+  ERRORS = ErrorsMessages;
+  
   constructor(private formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
-      email: [''],
-      password: ['']
+      email: ['', [
+        Validators.required,
+        Validators.pattern(Patterns.EMAIL_PATTERN)]
+      ],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20),
+        Validators.pattern(Patterns.PASSWORD_PATTERN)]
+      ]
     });
   }
 
