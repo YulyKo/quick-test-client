bf354ae0d14eac1da40f8c2b828a8a7fb6a13acc
YulyKo <yuliakotenko1.0@gmail.com>
2021-03-19 15:31:09 +0200

    guards/added logic for redirect user after login to /home

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/models/User';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { ErrorsMessages } from 'src/app/utils/ErrorsMessages';
@@ -17,13 +18,17 @@ export class LoginComponent implements OnInit {
   existUser: boolean;
   submited: boolean;
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute) {
     this.form = this.formBuilder.group({
-      email: ['', [
+      email: ['email@email.com', [
         Validators.required,
         Validators.pattern(Patterns.EMAIL_PATTERN)]
       ],
-      password: ['', [
+      password: ['1qASDFGHJKL', [
         Validators.required,
         Validators.minLength(8),
         Validators.maxLength(20),
@@ -48,9 +53,10 @@ export class LoginComponent implements OnInit {
             this.existUser = true;
             user.setAccessToken(res.accessToken);
             this.authService.setLoginStatus(true);
+            this.router.navigate(['/home']);
           },
           (error) => +error.status === 400 ? this.existUser = false : console.error('error')
-        )
+        );
     }, 1000);
   }
 
@@ -64,15 +70,15 @@ export class LoginComponent implements OnInit {
   checkExistingEmail(email: string): void {
     setTimeout(() => {
       this.authService.checkEmail(email)
-      .subscribe(
-        () => this.existEmail = false,
-        (error) => {
-          +error.status === 400 ? this.existEmail = true : console.log(error);
-          // пошта існує в базі, вхід доступний
-          const user = this.getUser();
-          this.loginUser(user);
-        },
-      )
+        .subscribe(
+          () => this.existEmail = false,
+          (error) => {
+            +error.status === 400 ? this.existEmail = true : console.log(error);
+            // пошта існує в базі, вхід доступний
+            const user = this.getUser();
+            this.loginUser(user);
+          },
+        );
     }, 1000);
   }
 }
