56f763a3d4106c6308c6602fa57622150bbc2584
YulyKo <yuliakotenko1.0@gmail.com>
2021-03-20 19:56:07 +0200

    Revert "Revert "Merge branch 'dev' into main""
    This reverts commit 816b7ebd3b8068b26e8da47ef515ea95191e6d3c.

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from 'src/app/components/auth/auth.component';
+import { LoginComponent } from 'src/app/components/auth/login/login.component';
+import { RegistrationComponent } from 'src/app/components/auth/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AuthComponent,
+    LoginComponent,
+    RegistrationComponent,
+    LoginViewComponent,
+    RegisterViewComponent,
+    ErrorMessageComponent,
   ],
   imports: [
     CommonModule
--- src/app/utils/UI/header/header.component.ts
+++ src/app/utils/UI/header/header.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  isOpen = false;
+  loginStatus: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
+  ngOnInit(): void {}
+
+  changeMenuVisibility(): void {
+    this.isOpen === true ? this.closeMenu() : this.openMenu();
+  }
+
+  openMenu(): void {
+    this.loginStatus = this.authService.getLoginStatus();
+    document.getElementById('page_header').style.backgroundColor = '#404040';
+    document.getElementById('header_menu').style.display = 'flex';
+    this.isOpen = true;
+  }
+
+  closeMenu(): void {
+    document.getElementById('page_header').style.backgroundColor = '#FFF';
+    document.getElementById('header_menu').style.display = 'none';
+    this.isOpen = false;
+  }
 }
