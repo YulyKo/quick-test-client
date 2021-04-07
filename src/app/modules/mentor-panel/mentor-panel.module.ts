e17782d5e7a4c8100b4ea4e1cb035848c85f4dfa
YulyKo <yuliakotenko1.0@gmail.com>
2021-04-07 11:49:11 +0300

    q-e/registred pipe in mentor-panel.module

--- src/app/modules/mentor-panel/mentor-panel.module.ts
+++ src/app/modules/mentor-panel/mentor-panel.module.ts
@@ -7,6 +7,7 @@ import { NewQuestionFormComponent } from 'src/app/components/mentor-panel/new-qu
 import { InputTextComponent } from 'src/app/utils/UI/form-elements/input-text/input-text.component';
 import { CreatingAnswersListFormComponent } from 'src/app/utils/UI/creating-answers-list-form/creating-answers-list-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TimeFilter } from 'src/app/utils/Time.pipe';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     NewQuestionFormComponent,
     InputTextComponent,
     CreatingAnswersListFormComponent,
+    TimeFilter,
   ],
   imports: [
     CommonModule,
