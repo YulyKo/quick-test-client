cb51192a1546f78592a6ed048cccbe125a15d223
YulyKo <yuliakotenko1.0@gmail.com>
2021-04-21 20:32:24 +0300

    test-essence/created list-of-questions component

---
+++ src/app/modules/questions/components/common/list-of-questions/list-of-questions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  template: 'app-list-of-questions',
+  templateUrl:'./list-of-questions.component.html',
+  styleUrls: ['./list-of-questions.component.sass']
+})
+export class ListOfQuestionsComponent {}
